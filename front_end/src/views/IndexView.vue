2e06bf33136d875a63c0d6c65cfac53ea05d3fa9
GitSteve1025 <3440346452@qq.com>
2024-05-20 16:14:25 +0800

    修改了 登出功能

--- front_end/src/views/IndexView.vue
+++ front_end/src/views/IndexView.vue
@@ -1,24 +1,25 @@
+<template>
+  <div>
+    欢迎进入问卷星系统
+  </div>
+  <div>
+    <el-button @click="logout()" type="danger" plain>退出登录</el-button>
+  </div>
+</template>
+
 <script setup>
   import {get} from "@/net";
   import {ElMessage} from "element-plus";
-  import router from "@/router/index.js";
+  import router from "@/router";
 
-  const logout=()=>{
-    get('/api/auth/logout',(message)=>{
+  const logout = () => {
+    get('/api/auth/logout', {},(message) => {
       ElMessage.success(message)
       router.push('/')
     })
   }
 </script>
 
-<template>
-  <div>
-    欢迎进入问卷星系统
-  </div>
-  <div>
-    <el-button @click="logout" type="danger" round>退出登录</el-button>
-  </div>
-</template>
 
 <style scoped>
 
