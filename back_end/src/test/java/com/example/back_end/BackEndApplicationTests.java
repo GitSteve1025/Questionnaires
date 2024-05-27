ce2820a899a78aa532da1bb2640bbd90ab5238da
GitSteve1025 <3440346452@qq.com>
2024-05-27 22:53:46 +0800

    增加了 QuestionnaireMapper 的测试

--- back_end/src/test/java/com/example/back_end/BackEndApplicationTests.java
+++ back_end/src/test/java/com/example/back_end/BackEndApplicationTests.java
@@ -1,15 +1,42 @@
 package com.example.back_end;
 
+
+import com.example.back_end.entity.Questionnaire.Questionnaire;
+import com.example.back_end.entity.auth.Account;
+import com.example.back_end.mapper.UserMapper;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.context.SecurityContextHolder;
+import  com.example.back_end.mapper.QuestionnaireMapper;
+
+
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
 
 @SpringBootTest
 class BackEndApplicationTests {
 
+    @Resource
+    UserMapper userMapper;
+
+    @Resource
+    QuestionnaireMapper questionnaireMapper;
+
+
     @Test
     void contextLoads() {
-       BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("123456"));
+        // 测试 questionnaireMapper 功能
+        Account account = new Account();
+        account.setId(1);
+        account.setUsername("admin");
+        account.setEmail("3440346452@qq.com");
+        account.setPassword("123456");
+
+        List<Questionnaire> questionnaires = questionnaireMapper.getQuestionnaires(account);
+        for (Questionnaire questionnaire : questionnaires) {
+            System.out.println(questionnaire);
+        }
     }
 }
