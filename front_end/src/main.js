d09f7087fafaef5bf68fcf952971c0f4cfcfc70d
QYXQYXQYX1 <1421992892@qq.com>
2024-05-30 15:54:06 +0800

    123456

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -7,11 +7,16 @@ import router from './router'
 import 'element-plus/dist/index.css'
 import axios from "axios";
 
+import ElementPlus from 'element-plus';
+
+
+
 const app = createApp(App)
 
 axios.defaults.baseURL='http://localhost:8080/'//地址绑定
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
-app.mount('#app')
+app.mount("#app");
