d9ea5e74e82878a35d068b2cc8cde979f6784fec
Colinrush1 <2446467601@qq.com>
2024-06-03 19:10:21 +0800

    单选、多选、填空页面实现

---
+++ front_end/src/components/Index/Choicepage.vue
@@ -0,0 +1,48 @@
+<script setup>
+import router from "@/router/index.js";
+import {post} from "@/net/index.js";
+import {ElMessage} from "element-plus";
+
+
+
+const Gosingle=()=>{
+  router.push('/index/singlechoice')
+}
+const Gomultiple=()=>{
+  router.push('/index/multiplechoice')
+}
+const Goblank=()=>{
+  router.push('/index/blankchoice')
+}
+
+</script>
+
+<template>
+  <div style="margin-top: 40px">
+    <el-button style="margin-left: 30px" @click="Gosingle">
+      单选题
+    </el-button>
+    <el-button style="margin-left: 30px" @click="Gomultiple">
+      多选题
+    </el-button>
+
+    <el-button style="margin-left: 30px" @click="Goblank">
+      填空题
+    </el-button>
+
+  </div>
+
+  <el-button style="margin-left: 100px;margin-top: 30px" @click="router.push('/index/show')">
+    返回
+  </el-button>
+
+
+  <div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ front_end/src/components/Index/Show.vue
@@ -0,0 +1,22 @@
+<script setup>
+import router from "@/router/index.js";
+
+
+const Gochoose=()=>{
+  router.push('/index/choicepage')
+}
+</script>
+
+<template>
+
+  <div style="margin-left: 100px;margin-top: 30px">这里显示标题</div>
+  <div style="margin-top: 20px;margin-left: 100px">这里显示对问卷的说明</div>
+
+  <div>
+    <el-button style="margin-top: 20px;margin-left: 100px" @click="Gochoose">添加题目</el-button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
