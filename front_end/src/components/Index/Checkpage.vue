0deb63b6a61a9ee2ed27529e37ccdd3861ddc009
Colinrush1 <2446467601@qq.com>
2024-06-27 11:51:49 +0800

    更新

---
+++ front_end/src/components/Index/Checkpage.vue
@@ -0,0 +1,133 @@
+<script setup>
+import router from "@/router/index.js";
+//import {ElMessage} from "element-plus";
+import {useRoute} from "vue-router";
+import {onMounted, reactive, ref} from "vue";
+import {get, post} from "@/net/index.js";
+import {Search} from "@element-plus/icons-vue";
+import {ElMessage} from "element-plus";
+const selectedRows=ref([]);//选中的行
+
+const route=useRoute()
+let Id=JSON.parse(route.query.params);
+const questions =reactive([]);
+
+const showData=()=>{
+  post('/questionnaires/get',{
+    questionnaireId:Id
+  }, (message)=> {
+    console.log(message)
+    for(let temp of message.choiceQuestions){
+      let id = temp.sequenceId;
+      questions.push(temp); // 将后端返回的问题数组存储到数组中
+      console.log(temp)
+    }
+    for(let temp of message.blankQuestions){
+      let id = temp.sequenceId;
+      questions.push(temp);
+      console.log(temp)
+    }
+    questions.sort((a, b)=>a.sequenceId - b.sequenceId);
+  },()=>{})
+
+}
+
+const Questionnaire = reactive({
+  questionnaireId:0,
+  title:'',
+  description:'',
+  choiceQuestions:[],
+  blankQuestions:[],
+  state:null,
+  createdTime:null,
+  starTime:null,
+  endTime:null
+})
+
+// 在组件挂载时获取问卷数据
+onMounted(() => {
+  showData();
+});
+
+const goBack = () => {
+  router.push({
+    path: '/index/choicepage',
+    query: {
+      params: Id
+    }
+  })
+};
+
+const updateSelectedCount = (question, choice) => {
+  if (choice.state) {
+    question.selectedCount++;
+  } else {
+    question.selectedCount--;
+  }
+};
+</script>
+
+<template>
+  <div>
+    <h1>问卷问题信息</h1>
+
+    <!--批量删除-->
+    <div>
+      <el-row :gutter="10" style="position: absolute;top:100px;">
+        <el-col :span="6" style="position: absolute;left:150px;">
+          <el-button style="margin-top: 265px" type="success" @click=goBack()>返回</el-button>
+        </el-col>
+      </el-row>
+    </div>
+    <!--表格组件-->
+    <el-table :data="questions"  border style="margin-top: 90px; margin-left: 40px; width:885px" >
+      <el-table-column prop="sequenceId" label="问题顺序" width="160" />
+      <el-table-column prop="title" label="题目" width="130"/>
+      <el-table-column prop="category" label="问题种类" width="100" />
+      <el-table-column label="问题选项"  >
+        <template #default="{ row }">
+          <div>
+            是否必填: {{ row.necessary ? '是' : '否' }}
+          </div>
+
+          <div v-if="row.category === 'SINGLE_CHOICE_QUESTION'">
+            <div v-for="(choice, index) in row.choices" :key="index">
+              {{ choice.content }}
+              <el-checkbox v-model="choice.state" @change="updateSelectedCount(row, choice)"/>
+            </div>
+          </div>
+
+          <div v-else-if="row.category === 'MULTIPLE_CHOICE_QUESTION'">
+            <div>
+              至少填：{{ row.minSelected }}
+            </div>
+
+            <div>
+              最多填：{{row.maxSelected}}
+            </div>
+
+            <div v-for="(choice, index) in row.choices" :key="index">
+              {{ choice.content }}
+              <el-checkbox v-model="choice.state" @change="updateSelectedCount(row, choice)" />
+            </div>
+            <div>
+              已选：{{ row.selectedCount }}
+            </div>
+          </div>
+
+          <div v-else-if="row.category === 'BLANK_QUESTION'">
+            <el-input v-model="row.blank.content" placeholder="请输入您的答案"></el-input>
+          </div>
+
+        </template>
+      </el-table-column>
+
+    </el-table>
+
+
+  </div>
+</template>
+
+<style scoped>
+/* 样式根据需要添加 */
+</style>
